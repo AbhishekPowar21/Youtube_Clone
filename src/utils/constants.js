e399f0367732158080d2c4de473360eebd9ba585
AbhishekPowar21 <abhipowar2143@gmail.com>
2025-04-26 00:09:44 +0530

    API UPDATE

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 // const GOOGLE_API_KEY = process.env.REACT_APP_GOOGLE_API_KEY;
-const GOOGLE_API_KEY = process.env.REACT_APP_GOOGLE_API_KEY;
+const GOOGLE_API_KEY = "AIzaSyAZrqAQNjKpMDUiG2Ow9sRTYbbR8FXs_p4";
 
 export const YOUTUBE_API =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
